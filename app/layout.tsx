483956fab9a4aaca7c64f44243def5001e9051c1
Brett Haas <bthaas15@gmail.com>
2026-07-13 12:55:15 -0400

    Use solid filled wing favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,8 +25,8 @@ export const metadata: Metadata = {
     images: ['/og-image.jpg'],
   },
   icons: {
-    icon: [{ url: '/favicon.png?v=3', type: 'image/png', sizes: '64x64' }],
-    shortcut: '/favicon.png?v=3',
+    icon: [{ url: '/favicon.svg?v=4', type: 'image/svg+xml' }],
+    shortcut: '/favicon.svg?v=4',
     apple: '/assets/wing-mark.png',
   },
 }
